9d1548e4c2f307544a853bb3e609de20d1c9f1f9
Matheus Andrell <matheusandrell27@gmail.com>
2024-03-18 22:40:30 -0300

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="\css\app.css">
+    <title>Triluna - Site</title>
+</head>
+
+<script src="https://kit.fontawesome.com/84534b9145.js" crossorigin="anonymous"></script>
+
+<body>
+    <div class="navbar">
+        <div class="navbar-logo">
+            <img src="logo.png" alt="Logo">
+        </div>
+        <div class="navbar-menu">
+            <ul>
+                <li><a href="#home">Home</a></li>
+                <li><a href="#services">Produtos</a></li>
+                <li><a href="#forma">Formas de Pagamento</a></li>
+                <li><a href="#about">Sobre Nós</a></li>
+            </ul>
+        </div>
+    </div>
+    <img src="img/img-background.png" alt="" class="background-img">
+    <section class="creative-section" id="services">
+        <div class="">
+            <h1>Creative</h1>
+            <h2>Process.</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore dignissimos laudantium
+                harum itaque at vel, est explicabo sequi, odit ut assumenda ullam magnam adipisci illo commodi maxime,
+                aut cupiditate eligendi.</p>
+            <a href=""><button class="button-primary">Entre em Contato</button></a>
+            <a href=""> Quer saber mais sobre nós?</a>
+        </div>
+    </section>
+
+    <section class="catalogos">
+        <div class="col-6">
+            <div class="catg bord1"></div>
+            <div class="catg bord2"></div>
+            <div class="catg bord3"></div>
+            <div class="catg bord4"></div>
+            <div class="catg bord5"></div>
+            <div class="catgg bord6"></div>
+        </div>
+    </section>
+    <section class="produtos">
+        <div class="col-4">
+            <div class="prod"></div>
+            <div class="prod"></div>
+            <div class="prod"></div>
+            <div class="prod"></div>
+        </div>
+    </section>
+    <section class="form-pag" id="forma">
+        <img src="img/formas-background.png" alt="" class="background-formas">
+        <div class="">
+            <h1>FORMAS DE<br> PAGAMENTO</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed aliquam quasi, quo dignissimos officia,
+                error animi consequatur quae corrupti, at expedita. Ratione ab officiis quisquam, asperiores reiciendis
+                quod natus voluptatem illo consequuntur est culpa blanditiis odio provident nisi vero. Iusto rem officia
+                ad qui debitis eaque hic deleniti molestias natus.</p>
+            <button class="button-primary">Lorem ipsum dolor sit amet.</button>
+        </div>
+    </section>
+    <section class="about-us" id="about">
+        <div class="absolute">
+            <div class="about-us-container">
+                <h1>SOBRE NÓS</h1>
+                <div class="about-us-text">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis nostrum pariatur doloribus quos
+                        ipsam,
+                        unde laborum aliquam ipsum sit accusantium? Eos quaerat, impedit quos hic cupiditate enim sed et
+                        earum,
+                        dolorem porro praesentium. Optio iste nostrum similique inventore iusto est sint hic nesciunt
+                        veritatis.
+                        Facere vel mollitia dolorum eligendi eveniet.</p>
+                </div>
+                <button class="button-primary">Lorem ipsum dolor sit amet.</button>
+            </div>
+            <div class="redes-about">
+                <i class="fa-brands fa-instagram insta"></i>
+                <i class="fa-brands fa-whatsapp wpp"></i>
+            </div>
+        </div>
+    </section>
+</body>
+
+</html>
---
+++ resources/views/product.blade.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="\css\app.css">
+    <title>Triluna - Site</title>
+</head>
+
+<script src="https://kit.fontawesome.com/84534b9145.js" crossorigin="anonymous"></script>
+
+<body>
+    <div class="navbar">
+        <div class="navbar-logo">
+            <img src="logo.png" alt="Logo">
+        </div>
+        <div class="navbar-menu">
+            <ul>
+                <li><a href="#home">Home</a></li>
+                <li><a href="#services">Produtos</a></li>
+                <li><a href="#forma">Formas de Pagamento</a></li>
+                <li><a href="#about">Sobre Nós</a></li>
+            </ul>
+        </div>
+    </div>
+    <section class="product-section">
+        <div class="product-container">
+            <div class="product-image-container">
+                <div class="product-image">
+                    <div></div>
+                </div>
+                <div class="more-image">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+            </div>
+            <div class="info-container">
+                <div class="info-product">
+                    <h2>Lorem ipsum dolor sit amet.</h2>
+                    <h1>Lorem ipsum dolor sit.</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde quasi nam harum laborum atque vel
+                        ad
+                        odio rem fugiat beatae!</p>
+                </div>
+                <div class="payment-product">
+                    <div class="price-and-discount">
+                        <h2>R$ 000.00</h2>
+                        <div class="desconto">
+                            <h1>00%</h1>
+                        </div>
+                    </div>
+                    <p>R$ 000.00</p>
+                </div>
+                <div class="button-product">
+                    <div class="button-quanty">
+                        <button class="quanty-btn decrease">-</button>
+                        <input type="text" class="quanty" value="1">
+                        <button class="quanty-btn increase">+</button>
+                    </div>
+                    <button class="button-primary ml"><i class="fa-solid fa-cart-shopping shopp"></i>Adicionar ao Carrinho</button>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+
+
+<script>
+    document.querySelector('.increase').addEventListener('click', function() {
+        var quantyInput = document.querySelector('.quanty');
+        var currentValue = parseInt(quantyInput.value, 10);
+        quantyInput.value = isNaN(currentValue) ? 1 : currentValue + 1;
+    });
+
+    document.querySelector('.decrease').addEventListener('click', function() {
+        var quantyInput = document.querySelector('.quanty');
+        var currentValue = parseInt(quantyInput.value, 10);
+        quantyInput.value = (isNaN(currentValue) || currentValue <= 1) ? 1 : currentValue - 1;
+    });
+</script>
